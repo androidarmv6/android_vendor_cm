31149f0932c9b9908e70912b9e73cf69f27a96c0
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2013-01-03 22:00:44 -0500

    Add missing packages to mini config
    For some reason this thing was missing Apollo and DSP Manager.
    Not exactly fun for me.....
    
    Change-Id: I08d9aa955d7a36f895f044fab4045a80ef60077e

--- config/mini.mk
+++ config/mini.mk
@@ -3,4 +3,13 @@ $(call inherit-product, vendor/cm/config/tiny.mk)
 # Mini config (Required /system size: min=175MB)
 
 PRODUCT_PACKAGES += \
+    Trebuchet \
+    DSPManager \
+    libcyanogen-dsp \
+    audio_effects.conf \
+    Apollo \
+    CMFileManager \
+    Development \
+    SoundRecorder \
+    Basic \
     LockClock
