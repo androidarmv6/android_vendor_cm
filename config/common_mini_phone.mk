5cf71200b679fa2ce9697602df56985452790e05
AndroidMeda <hephappy@gmail.com>
2013-02-14 18:59:38 +0200

    Use our common_mini.mk and no cm ring tones

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,17 +1,17 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/cm/config/common_mini.mk)
 
 # Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
+include frameworks/base/data/sounds/OldAudio.mk
 
 # Include CM audio files
-include vendor/cm/config/cm_audio.mk
+#include vendor/cm/config/cm_audio.mk
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=CyanTone.ogg \
-    ro.config.notification_sound=CyanMessage.ogg \
-    ro.config.alarm_alert=CyanAlarm.ogg
+    ro.config.ringtone=Ring_Digital_02.ogg \
+    ro.config.notification_sound=F1_New_SMS.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
 
 PRODUCT_PACKAGES += \
   Mms
