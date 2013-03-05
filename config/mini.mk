e1e34942904ce02c0484b2e9c7b1f7ef3818d187
AndroidMeda <hephappy@gmail.com>
2013-03-05 16:51:29 +0200

    Add Terminal emulator to mini.mk
    Change-Id: I5ee77323433b0904a400b1041fab415a4e82702f

--- config/mini.mk
+++ config/mini.mk
@@ -1,4 +1,10 @@
 # Mini config (Required /system size: min=175MB)
+
+# Terminal Emulator
+PRODUCT_COPY_FILES +=  \
+    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
+    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+
 PRODUCT_PACKAGES += \
     Trebuchet \
     DSPManager \
