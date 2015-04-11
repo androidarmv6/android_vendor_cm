ac16382d194983e074c2d6605f09f2ab39b10335
olivieer <yves.olli@gmail.com>
2015-04-11 22:06:37 +0200

    cm: update libjackpal version in mini config too
    Change-Id: I23dd1083f4cbf51a6052e3ef8b305a0509dc53b5

--- config/mini.mk
+++ config/mini.mk
@@ -1,9 +1,10 @@
 # Mini config (Required /system size: min=175MB)
 
 # Terminal Emulator
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+#PRODUCT_COPY_FILES +=  \
+#    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
+#    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so \
+#    vendor/cm/proprietary/lib/armeabi/libjackpal-termexec2.so:system/lib/libjackpal-termexec2.so
 
 PRODUCT_PACKAGES += \
     DSPManager \
